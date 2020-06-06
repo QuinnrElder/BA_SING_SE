315992d70d2951a98f1b4557e582cd1344c570a7
Quinn Elder <qre.turing@gmail.com>
2020-06-06 12:54:34 -0600

    :crystal_ball: Add componentDidMout for rooms and bookings

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import { Route, Switch } from "react-router-dom";
+import { fetchBookings, fetchRooms, fetchUserBookings } from '../ApiFetchMethods/ApiFetchMethods'
 
 import Login from '../Login/Login';
 import UserPage from '../UserPage/UserPage';
@@ -13,9 +14,20 @@ class App extends React.Component {
     this.state = {
       user: {
         userBookings: [],
+        roomsStayedIn: [],
       },
       allUsers: [],
+      allBookings: [],
+      allRooms: [],
     }
+  }
+
+  componentDidMount = async () => {
+    const allBookings = await fetchBookings()
+    this.setState({ allBookings: [...this.state.allBookings, ...allBookings] })
+    
+    const allRooms = await fetchRooms()
+    this.setState({ allRooms: [...this.state.allRooms, ...allRooms] })
   }
 
   getUser = (user) => {
@@ -23,13 +35,33 @@ class App extends React.Component {
     this.setState({ user: user.name })
     this.setState({ allUsers: [...user.allUsers] })
   } else {
-    this.setState({ user: {id: user.id, name: user.name, userBookings: []} })
+    const userBookings = this.state.allBookings.filter(booking => booking.userID === user.id)
+    const roomsStayedIn = this.findRoomStayedIn(userBookings)
+    this.setState({ user: {
+      id: user.id, 
+      name: user.name, 
+      userBookings: userBookings, 
+      roomsStayedIn: roomsStayedIn
+      } 
+    })
     this.setState({ allUsers: [...user.allUsers] })
   }
   }
 
+  findRoomStayedIn = (userBookings) => {
+    let myRooms = this.state.allRooms.reduce((acc, room) => {
+      userBookings.forEach(booking => {
+        if (booking.roomNumber === room.number) {
+          acc.push(room)
+        }
+      })
+      return acc
+    }, [])
+    return myRooms
+  }
+
   render() {
-console.log(this.state.user)
+    console.log(this.state.user)
     return (
       <main className="App">
         <Switch >
@@ -39,7 +71,12 @@ console.log(this.state.user)
             render={() => {
               return (
               <div className='mainImage'>
-                <ManagerPage user={this.state.user} allUsers={this.state.allUsers}/>
+                <ManagerPage
+                user={this.state.user}
+                allUsers={this.state.allUsers} 
+                allRooms={this.state.allRooms} 
+                allBookings={this.state.allBookings} 
+                />
               </div>
               )
           }}
