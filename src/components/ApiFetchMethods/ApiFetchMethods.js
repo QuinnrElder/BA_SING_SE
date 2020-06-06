dc5bbcc3d661c5811bad4b9bbe2cffbb5f8ba3f3
Quinn Elder <qre.turing@gmail.com>
2020-06-06 11:01:22 -0600

    :crystal_ball: Add a new FetchRequest for bookings to Manager and User Pages

--- src/components/ApiFetchMethods/ApiFetchMethods.js
+++ src/components/ApiFetchMethods/ApiFetchMethods.js
@@ -5,3 +5,16 @@ const usersApiData = await promise.json()
 
 return usersApiData.users
 }
+
+
+export const fetchBookings = async (user) => {
+  const promise = await fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
+  const usersApiData = await promise.json()
+  return usersApiData.bookings
+}
+
+export const fetchUserBookings = async (user) => {
+  const promise = await fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
+  const usersApiData = await promise.json()
+  return usersApiData.bookings.filter(booking => booking.userID === user.id)
+}
