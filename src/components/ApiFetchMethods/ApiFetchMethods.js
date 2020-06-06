54af43f6abff0ec4ab0f52524b94cdec274b72dd
Quinn Elder <qre.turing@gmail.com>
2020-06-06 13:52:47 -0600

    :crystal_ball: Refactor fetch call for Bookings

--- src/components/ApiFetchMethods/ApiFetchMethods.js
+++ src/components/ApiFetchMethods/ApiFetchMethods.js
@@ -2,21 +2,13 @@
 export const fetchUsers = async () => {
 const promise = await fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
 const usersApiData = await promise.json()
-
 return usersApiData.users
 }
-
 
 export const fetchBookings = async (user) => {
   const promise = await fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
   const usersApiData = await promise.json()
   return usersApiData.bookings
-}
-
-export const fetchUserBookings = async (user) => {
-  const promise = await fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
-  const usersApiData = await promise.json()
-  return usersApiData.bookings.filter(booking => booking.userID === user.id)
 }
 
 export const fetchRooms = async () => {
