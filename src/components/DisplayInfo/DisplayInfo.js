95d159d8e13eb6bb1f3a614afe2cde2bafde8a0b
Quinn Elder <qre.turing@gmail.com>
2020-06-06 12:09:32 -0600

    :crystal_ball: Add the fuunctionality to display all the users bookings

--- src/components/DisplayInfo/DisplayInfo.js
+++ src/components/DisplayInfo/DisplayInfo.js
@@ -2,12 +2,31 @@ import React from 'react'
 import './DisplayInfo.css'
 // import PropTypes from 'prop-types'
 
-const DisplayInfo = () => {
-  
+const DisplayInfo = ({ user }) => {
+  console.log(user)
+  const displayBookings = () => {
+    return user.userBookings.map(booking => {
+      return (
+        <div className='each-booking-container'>
+          <p className='booking-text'> Confirmation <span className='insertedText'>{booking.id}</span></p>
+          <p className='booking-text'> Room #<span className='insertedText'>{booking.roomNumber}</span></p>
+          <p className='booking-text'> Date #<span className='insertedText'>{booking.date}</span></p>
+        </div>
+      )
+    })
+  }
+
+  const findTotalSpent = () => {
+    
+  }
+
   return (
-    <div>
-      <p>hi</p>
+  <div className='display-info'>
+    <p className='bookings-title'><span>`Total $${}`</span>BOOKINGS</p>
+    <div className='display-info-container'>
+      {displayBookings()}
     </div>
+  </div>
   )
 }
 export default DisplayInfo
