70ba92cf8ae80013f7e68569379330ce12b970df
Quinn Elder <qre.turing@gmail.com>
2020-06-06 11:05:45 -0600

    :crystal_ball: Add a new FetchRequest for bookings in UserPage

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import './UserPage.css'
+// import PropTypes from 'prop-types'
 
-const UserPage = ({ user }) => {
+import { fetchUserBookings } from '../ApiFetchMethods/ApiFetchMethods'
+import DisplayInfo from '../DisplayInfo/DisplayInfo'
 
-  return (
-    <div className='user-page' >
-      <h3 className='user-page-header'>{`Welcome ${user.name}` }</h3>
-    </div>
-  )
+class UserPage extends React.Component {
+  constructor(props)  {
+    super(props)
+    this.state = {
+      user: props.user
+    }
+  }
+
+  componentDidMount = async () => {
+    const userBookings = await fetchUserBookings(this.state.user)
+    this.setState({ user: {...this.state.user, userBookings: userBookings }})
+  }
+
+  render() {
+    return (
+      <div className='user-page' >
+        <h3 className='user-page-header'>{ `Welcome ${this.state.user.name}` }</h3>
+        {/* render the search for room by date and roomType form */}
+        <DisplayInfo  user={this.state.user} />
+      </div>
+    )
+  }
 }
 
 export default UserPage
