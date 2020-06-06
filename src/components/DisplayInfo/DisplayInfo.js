78bd53067380fd9ac3d356cc87ae78078916d2ca
Quinn Elder <qre.turing@gmail.com>
2020-06-06 14:54:30 -0600

    :crystal_ball: Refactor for UserPage only

--- src/components/DisplayInfo/DisplayInfo.js
+++ src/components/DisplayInfo/DisplayInfo.js
@@ -3,7 +3,7 @@ import './DisplayInfo.css'
 // import PropTypes from 'prop-types'
 
 const DisplayInfo = ({ user }) => {
-  console.log(user)
+
   const displayBookings = () => {
     return user.userBookings.map(booking => {
       return (
@@ -17,23 +17,23 @@ const DisplayInfo = ({ user }) => {
   }
 
   const findTotalSpent = () => {
-    let number = user.roomsStayedIn.reduce((acc, room) => {
-      acc += room.costPerNight
-      return acc
-    }, 0)
-    return number.toFixed(2)
+      let number = user.roomsStayedIn.reduce((acc, room) => {
+        acc += room.costPerNight
+        return acc
+      }, 0)
+      return number.toFixed(2)
   }
 
   return (
-  <div className='display-info'>
-    <div className='bookings-title-container'>
-    <p className='bookings-cost'>{`Total $ ${findTotalSpent()}`}</p>
-    <p className='bookings-title'>BOOKINGS</p>
+    <div className='display-user-info'>
+      <div className='bookings-title-container'>
+      <p className='bookings-cost'>{`Total $ ${findTotalSpent()}`}</p>
+      <p className='bookings-title'>BOOKINGS</p>
+      </div>
+      <div className='display-info-container'>
+        {displayBookings()}
+      </div>
     </div>
-    <div className='display-info-container'>
-      {displayBookings()}
-    </div>
-  </div>
-  )
+    )
 }
 export default DisplayInfo
