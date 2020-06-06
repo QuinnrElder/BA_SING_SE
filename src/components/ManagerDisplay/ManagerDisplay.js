7aa733feb504e3a463e28ce5a5bb6ad547074f1a
Quinn Elder <qre.turing@gmail.com>
2020-06-06 16:55:36 -0600

    :crystal_ball: Add functionality to render the amount made on the day & rooms available for the day

--- src/components/ManagerDisplay/ManagerDisplay.js
+++ src/components/ManagerDisplay/ManagerDisplay.js
@@ -1,17 +1,52 @@
 import React from 'react'
 import './ManagerDisplay.css'
 
-const ManagerDisplay = ({user, allUsers, allRooms, allBookings}) => {
+const ManagerDisplay = ({user, allUsers, allRooms, allBookings, date}) => {
+  
+  const getTotalRevenue = () => {
+    let newDate = getDateSyntax(date)
+    let matchingBookings = allBookings.filter(booking => booking.date === newDate);
+    let profits = matchingBookings.reduce((acc, match) => {
+      this.rooms.forEach(room => {
+        if (room.number === match.roomNumber) {
+          acc = room.costPerNight + acc
+        }
+      })
+      return acc
+    }, 0)
+    return profits
+  }
+
+  const getDateSyntax = (date) => {
+    let dateArray = date.split('/');
+    let day = dateArray[1];
+    let month = dateArray[0];
+    let year = dateArray[2];
+    if (month <= 9) {
+      let unformattedDate = `${year}/0${month}/${day}`;
+      return unformattedDate
+    } else {
+      let unformattedDate = `${year}/${month}/${day}`;
+      return unformattedDate
+    }
+  }
+
+  const findNumberOfRoomsAvailable = () => {
+    let newDate = getDateSyntax(date)
+    let matchingBookings = allBookings.filter(booking => booking.date === newDate)
+    let number = (allRooms.length - matchingBookings.length).toFixed(0)
+    return number
+  }
 
 return (
   <div className='display-manager-info'>
 
     <div className='info-container'>
-      <p className='info-title'>Hello</p>
+      <p className='info-title'>{`$ ${getTotalRevenue()}`}</p>
     </div>
 
     <div className='info-container'>
-      <p className='info-title'>World</p>
+      <p className='info-title'>{`Rooms Available: ${findNumberOfRoomsAvailable()}`}</p>
     </div>
 
     <div className='info-container'>
