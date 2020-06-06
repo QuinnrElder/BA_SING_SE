97789dfdf33ec272418e41bdece6e45a1d3ff539
Quinn Elder <qre.turing@gmail.com>
2020-06-06 11:04:02 -0600

    :crystal_ball: Add async componentDidMount for bookings

--- src/components/ManagerPage/ManagerPage.js
+++ src/components/ManagerPage/ManagerPage.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import './Manager.css'
+// import PropTypes from 'prop-types'
 
-const ManagerPage = ({ user }) => {
+import { fetchBookings } from '../ApiFetchMethods/ApiFetchMethods'
 
+import DisplayInfo from '../DisplayInfo/DisplayInfo';
+
+class ManagerPage extends React.Component {
+constructor(props) {
+  super(props)
+  this.state = {
+    name: 'manager',
+    allBookings: [],
+  }
+}
+
+componentDidMount = async () => {
+  const bookings = await fetchBookings()
+  this.setState({ allBookings: bookings })
+}
+
+  render() {
   return (
     <div >
-      <h1 >{`Welcome ${user.name}` }</h1>
+      {/* render the search by user form */}
+      <DisplayInfo />
     </div>
   )
+  }
 }
 
 export default ManagerPage
