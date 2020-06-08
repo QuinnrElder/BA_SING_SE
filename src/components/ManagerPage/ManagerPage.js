fcc8e16e8640aa0780adc02f0de634a56bf970cb
Quinn Elder <qre.turing@gmail.com>
2020-06-07 21:51:43 -0600

    :crystal_ball: Add functionality for ManagerPage

--- src/components/ManagerPage/ManagerPage.js
+++ src/components/ManagerPage/ManagerPage.js
@@ -4,42 +4,118 @@ import './ManagerPage.css'
 
 import ManagerDisplay from '../ManagerDisplay/ManagerDisplay'
 import ManagerForm from '../ManagerForm/ManagerForm'
-import UserPage from '../UserPage/UserPage.test'
+import CustomerForm from '../CustomerForm/CustomerForm'
+import AvailableRooms from '../AvailableRooms/AvailableRooms'
+
 
 class ManagerPage extends React.Component {
   constructor( props ) {
     super( props )
     this.state = {
-      user: props.user,
-      allUsers: props.allUsers, 
-      allRooms: props.allRooms, 
-      allBookings: props.allBookings, 
-      date: props.date,
       hasSearched: false,
+      hasBeenSearched: false,
       userName: '',
+      searchInput: {},
+      user: props.user,
+      availableRooms: [],
+      allRooms: props.allRooms,
+      date: props.date,
+      allBookings: props.allBookings,
     }
   }
 
-  getCustomerFormInput = (userName) => {
-    this.setState({ userName: userName, hasSearched: true})
+  getManagerFormInput = (userName) => {
+    this.setState({ userName: userName})
+    this.setState({  hasBeenSearched: true})
   }
 
-  // here is where I want to find the searched user
-  // render the user page with all the users info attached
-  // running into an issue with the allUsers data not having all the bookings for each user 
-  // searchForUser = () => {
-  //   let searchedUser = this.state.allUsers.find( user => user.name === this.state.userName)
-  //   return <UserPage />
-  // }
+  getCustomerFormInput = (date, filterType) => {
+    this.setState({ searchInput: {date: date, filterType: filterType}, hasSearched: true})
+  }
+
+  findingRoomsAvailableToday = () => {
+    if (this.state.searchInput.filterType === '') {
+      let bookingNums = this.getAllRoomNumbersBookedToday()
+      let availableRooms = this.findAvailableRoomNumbers(bookingNums)
+      return this.displayAvailableRoomsInfo(availableRooms)
+    } else {
+      let bookingNums = this.getAllRoomNumbersBookedToday()
+      let availableRooms = this.findAvailableRoomNumbers(bookingNums)
+      let displayFilteredRoomType = this.findFilteredRoomType(availableRooms)
+      return this.displayAvailableRoomsInfo(displayFilteredRoomType)
+    }
+  }
+
+  getAllRoomNumbersBookedToday = () => {
+    let booked = []
+    this.state.allBookings.forEach(booking => {
+      if (booking.date === this.state.searchInput.date) {
+        booked.push(booking.roomNumber)
+      }
+    })
+    return booked
+  }
+
+  findAvailableRoomNumbers = (bookingNums) => {
+    let newHotel = this.props.allRooms
+    bookingNums.forEach(booking => {
+      newHotel.forEach(room => {
+        if (room.number === booking) {
+          const value = newHotel.indexOf(room)
+          newHotel.splice(value, 1)
+        }
+      })
+    })
+    return newHotel
+  }
+
+  findFilteredRoomType = (availableRooms) => {
+    let filtered = []
+    availableRooms.forEach(room => {
+      if (room.roomType === this.props.searchInput.filterType) {
+        filtered.push(room)
+        return filtered
+      }
+    })
+    return filtered
+  }
+
+ displayAvailableRoomsInfo = (availableRooms) => {
+    return availableRooms.map(room => {
+      return (
+        <AvailableRooms key={Math.floor(Math.random() * 10000000) + 1} room={room} date={this.props.searchInput.date} user={this.props.user}/>
+        ) 
+    })
+  }
+
+  searchForUser = () => {
+    console.log(typeof this.state.userName)
+
+    let newUser = this.props.allUsers.find(user => {
+     if(user.name === this.state.userName) {
+       console.log(user)
+       return user
+     }
+    })
+    return (
+      <div className='user-page-for-manager' >
+        <h3 className='user-page-header-for-manager'>{ this.state.userName }</h3>
+        <div className='main-form-and-res-container-for-manager'>
+        <CustomerForm getCustomerFormInput={this.getCustomerFormInput}/>
+        {this.state.hasSearched && <div className='form-test-for-manager'>{this.findingRoomsAvailableToday()}</div>}
+        </div>
+      </div>
+    )
+  }
 
   render() {
     return (
-      <div className='user-page'>
-        <div className='main-form-and-res-containers'>
-        <ManagerForm getCustomerFormInput={this.getCustomerFormInput}/>
-        {this.state.hasSearched && <div className='form-test'>{}</div>}
+      <div className='manager-page'>
+        <div className='main-form'>
+        <ManagerForm getManagerFormInput={this.getManagerFormInput}/>
+        {this.state.hasBeenSearched && <div className='form-test'>{this.searchForUser()}</div>}
         </div>
-        <ManagerDisplay user={this.state.user} allUsers={this.state.allUsers} allRooms={this.state.allRooms} allBookings={this.state.allBookings} date={this.state.date} />
+        <ManagerDisplay user={this.props.user} allUsers={this.props.allUsers} allRooms={this.props.allRooms} allBookings={this.props.allBookings} date={this.props.date} />
       </div>
     )
   }
