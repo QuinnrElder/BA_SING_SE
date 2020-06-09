e3bc7267ce5107a87ecbebffc508e32143012104
Quinn Elder <qre.turing@gmail.com>
2020-06-09 16:01:27 -0600

    :bar_chart: Refactor for AvailableRooms test

--- src/components/AvailableRooms/AvailableRooms.js
+++ src/components/AvailableRooms/AvailableRooms.js
@@ -16,14 +16,19 @@ class AvailableRooms extends React.Component {
 
   render() {
   return (
-    <section className="singleBooking"> 
+    <section className="singleBooking" data-testid='available-rooms'> 
       <p className="booking-info">Room Number: {this.props.room.number}</p>
       <p className="booking-info">Room Type: {this.props.room.roomType}</p>
       <p className="booking-info">Has Bidet: {this.props.room.bidet}</p>
       <p className="booking-info">Bed Size: {this.props.room.bedSize}</p>
       <p className="booking-info">Number Of Beds: {this.props.room.numBeds}</p>
       <p className="booking-info">Cost Per Night: ${this.props.room.costPerNight}</p>
-      <button onClick={this.handleEvent} className="book-room-btn" id={this.props.room.number}>BOOK NOW</button>
+      <button 
+      onClick={this.handleEvent} 
+      className="book-room-btn" 
+      id={this.props.room.number}
+      data-testid={this.props.room.number}
+      >BOOK NOW</button>
     </section>
   )
   }
